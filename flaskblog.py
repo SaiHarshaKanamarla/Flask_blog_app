5d573f9493f7d92215137a4cc0e0cf3188f3914e
sidharsha <siddharth.landers@gmail.com>
2022-10-03 10:30:00 -0400

    first commit

---
+++ flaskblog.py
@@ -0,0 +1,36 @@
+from flask import Flask
+# flask package to render template in routes..to render static pages essentially
+from flask import render_template
+
+posts = [
+    {
+        'author': 'Sid Harsha',
+        'title': 'Blog post 1',
+        'content': 'This is the first post',
+        'date': 'Aug 3, 2022'
+    },
+    {
+        'author': 'Harsha',
+        'title': 'Blog post 2',
+        'content': 'This is the Second post',
+        'date': 'Aug 9, 2022'
+    }
+]
+
+# __name__ is name of the module..tells flask where to look for templates and stuff
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/home")  # decorators
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title="About")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
